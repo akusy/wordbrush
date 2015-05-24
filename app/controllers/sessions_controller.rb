d63ce415c8adfee100bf8e5a4b75db0909fef280
Arkadiusz Kusy <kusyarkadiusz@gmail.com>
2015-05-24 17:32:45 +0100

    Add users signing in form

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,6 +6,18 @@ class SessionsController < AuthenticationsController
     end
   end
 
+  def create
+    if @user.answer == params[:answer]
+      session[:user_id] = @user.id
+      redirect_to root_path
+    else
+      redirect_to(new_sessions_path(
+        activation_code: params[:activation_code],
+        errors: "Wrong answer. Try again."
+      ))
+    end
+  end
+
   def destroy
     session.delete(:user_id)
 
