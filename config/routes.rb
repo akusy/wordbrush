a79a621579a6a4e401c6f58924f6bd49f31a0a2e
Arkadiusz Kusy <kusyarkadiusz@gmail.com>
2015-05-24 23:03:10 +0100

    Add users index action

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,6 @@ Rails.application.routes.draw do
   delete '/sessions' => 'sessions#destroy', as: 'logout'
   resources :sessions, only: :create
 
-  resources :users, only: [:new, :create]
+  resources :users, only: [:new, :create, :index]
 
 end
