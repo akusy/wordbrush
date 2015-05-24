3d0ceeafa625a0dd4ba195b77e9980bbe6c220a3
Arkadiusz Kusy <kusyarkadiusz@gmail.com>
2015-05-24 17:33:57 +0100

    Add sessions create to routes and root path

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
+  root :to => "users#new"
 
   get 'registrations/:activation_code' => 'registrations#new', as: 'new_registrations'
   resources :registrations, only: :create
 
   get '/sessions/:activation_code' => 'sessions#new', as: 'new_sessions'
   delete '/sessions' => 'sessions#destroy', as: 'logout'
+  resources :sessions, only: :create
 
   resources :users, only: [:new, :create]
+
 end
