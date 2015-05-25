22293f0221c996d16e7cd7522067bec60826a982
Arkadiusz Kusy <kusyarkadiusz@gmail.com>
2015-05-25 21:26:35 +0100

    Finish sessions_controller_spec

--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -17,5 +17,36 @@ describe SessionsController do
 
       it { is_expected.to redirect_to(new_registrations_path(activation_code: user.activation_code)) }
     end
+
+    context "When activation_code is incorrect" do
+      before { get :new, activation_code: "incorrect_code" }
+
+      it { is_expected.to respond_with(:not_found) }
+    end
+  end
+
+  describe "#create" do
+    let(:user) { create :user, active: true }
+
+    context "When user answered the question" do
+      before { post :create, answer: user.answer, activation_code: user.activation_code }
+
+      it "saves user ID to session" do
+        expect(session[:user_id]).to eq user.id
+      end
+
+      it { is_expected.to redirect_to users_path }
+    end
+
+    context "When user did not answer the question" do
+      before { post :create, answer: "?", activation_code: user.activation_code }
+
+      it "redirects back to the login form" do
+        is_expected.to redirect_to(new_sessions_path(
+          activation_code: user.activation_code,
+          errors: "Wrong answer. Try again."
+        ))
+      end
+    end
   end
 end
